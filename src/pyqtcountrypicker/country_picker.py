53527f5afe70747c0318f393647c97fc7021000c
Niklas Henning <niklashenningbusiness@gmail.com>
2024-09-24 21:04:31 +0200

    Implement country name customization

--- src/pyqtcountrypicker/country_picker.py
+++ src/pyqtcountrypicker/country_picker.py
@@ -1,5 +1,6 @@
 from qtpy.QtWidgets import QWidget, QComboBox
 from qtpy.QtGui import QIcon
+from qtpy.QtCore import Qt
 from .countries import countries
 from .os_utils import OSUtils
 
@@ -15,24 +16,47 @@ class CountryPicker(QComboBox):
         super(CountryPicker, self).__init__(parent)
 
         self.countries = countries.copy()
+        self.current_country = next(iter(self.countries.keys()))
 
-        for country_code, country_name in countries.items():
+        for country_code, country_name in self.countries.items():
             self.addItem(
                 QIcon(OSUtils.get_current_directory() + '/flags/' + country_code + '.png'),
                 country_name
             )
 
     def getCurrentCountry(self) -> str:
-        pass
+        return self.current_country
 
-    def setCurrentCountry(self):
-        pass
+    def setCurrentCountry(self, country_code: str):
+        self.current_country = country_code
+        self.setCurrentIndex(self.findText(self.countries[country_code]))
 
     def getCountryName(self, country_code) -> str:
-        pass
+        return self.countries[country_code]
 
     def setCountryName(self, country_code: str, country_name: str):
-        pass
+        item_index = self.findText(self.countries[country_code])
+        self.setItemText(item_index, country_name)
+        self.model().sort(self.modelColumn(), Qt.SortOrder.AscendingOrder)
+        self.countries[country_code] = country_name
+
+    def getCountryNames(self) -> dict[str, str]:
+        return self.countries
 
     def setCountryNames(self, country_names: dict[str, str]):
-        pass
+        for country_code, country_name in country_names.items():
+            item_index = self.findText(self.countries[country_code])
+            self.setItemText(item_index, country_name)
+            self.countries[country_code] = country_name
+        self.model().sort(self.modelColumn(), Qt.SortOrder.AscendingOrder)
+
+    def resetCountryNames(self):
+        self.clear()
+        self.countries = countries.copy()
+        for country_code, country_name in self.countries.items():
+            self.addItem(
+                QIcon(OSUtils.get_current_directory() + '/flags/' + country_code + '.png'),
+                country_name
+            )
+        self.model().sort(self.modelColumn(), Qt.SortOrder.AscendingOrder)
+        self.setCurrentCountry(self.current_country)
