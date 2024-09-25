4162ba428995eb4d0ecab78951159807afcb58d4
Niklas Henning <niklashenningbusiness@gmail.com>
2024-09-25 18:29:13 +0200

    Add option to set custom flags

--- src/pyqtcountrypicker/country_picker.py
+++ src/pyqtcountrypicker/country_picker.py
@@ -20,6 +20,7 @@ class CountryPicker(QComboBox):
         super(CountryPicker, self).__init__(parent)
 
         self.countries = countries.copy()
+        self.country_flags = self.__get_default_country_flags()
         self.flag_icons_enabled = True
         self.filter = None
         self.filtered_countries = set()
@@ -28,7 +29,6 @@ class CountryPicker(QComboBox):
         self.blocking_signals = False
 
         self.__update_dropdown_items()
-
         self.currentTextChanged.connect(self.__current_text_changed)
 
     def getCurrentCountry(self) -> str:
@@ -43,6 +43,9 @@ class CountryPicker(QComboBox):
         self.setCurrentIndex(self.findText(self.countries[country_code]))
 
     def getCountryName(self, country_code) -> str:
+        country_code = country_code.lower()
+        if country_code not in self.countries:
+            return ''
         return self.countries[country_code]
 
     def setCountryName(self, country_code: str, country_name: str):
@@ -77,6 +80,34 @@ class CountryPicker(QComboBox):
         self.flag_icons_enabled = enabled
         self.__update_dropdown_items()
 
+    def getCountryFlag(self, country_code: str) -> QIcon:
+        country_code = country_code.lower()
+        if country_code not in self.countries:
+            return QIcon()
+        return self.country_flags[country_code]
+
+    def setCountryFlag(self, country_code: str, icon: QIcon):
+        country_code = country_code.lower()
+        if country_code not in self.countries:
+            return
+        self.country_flags[country_code] = icon
+        self.__update_dropdown_items()
+
+    def getCountryFlags(self) -> dict[str, QIcon]:
+        return self.country_flags
+
+    def setCountryFlags(self, country_flags: dict[str, QIcon]):
+        for country_code, flag_icon in country_flags.items():
+            country_code = country_code.lower()
+            if country_code not in self.countries:
+                continue
+            self.country_flags[country_code] = flag_icon
+        self.__update_dropdown_items()
+
+    def resetCountryFlags(self):
+        self.country_flags = self.__get_default_country_flags()
+        self.__update_dropdown_items()
+
     def getFilter(self) -> FilterType | None:
         return self.filter
 
@@ -102,10 +133,7 @@ class CountryPicker(QComboBox):
                 continue
 
             if self.flag_icons_enabled:
-                self.addItem(
-                    QIcon(OSUtils.get_current_directory() + '/flags/' + country_code + '.png'),
-                    country_name, userData=country_code
-                )
+                self.addItem(self.country_flags[country_code], country_name, userData=country_code)
             else:
                 self.addItem(country_name, userData=country_code)
 
@@ -117,3 +145,9 @@ class CountryPicker(QComboBox):
         if not self.blocking_signals:
             self.current_country = self.currentData()
             self.countryChanged.emit(self.current_country)
+
+    def __get_default_country_flags(self) -> dict[str, QIcon]:
+        return {
+            country_code: QIcon(OSUtils.get_current_directory() + '/flags/{}.png'.format(country_code))
+            for country_code in countries.keys()
+        }
