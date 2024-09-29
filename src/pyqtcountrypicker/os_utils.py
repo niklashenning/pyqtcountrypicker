87b87fb4bd4c4ebb65844bb6981340ab73d5bf4e
Niklas Henning <niklashenningbusiness@gmail.com>
2024-09-29 12:42:05 +0200

    Add documentation and comments

--- src/pyqtcountrypicker/os_utils.py
+++ src/pyqtcountrypicker/os_utils.py
@@ -5,4 +5,9 @@ class OSUtils:
 
     @staticmethod
     def get_current_directory() -> str:
+        """Get the current directory path
+
+        :return: directory path
+        """
+
         return os.path.dirname(os.path.realpath(__file__))
